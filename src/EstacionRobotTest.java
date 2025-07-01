0cc02d6aef14a76514f06b482c04d934bf0358f1
Martin Didolich <marindidolich2012@gmail.com>
2025-07-01 16:02:45 -0300

    test

--- src/EstacionRobotTest.java
+++ src/EstacionRobotTest.java
@@ -1,14 +1,12 @@
 import static org.junit.Assert.*;
 
 import Item.Item;
-import Item.TipoItem;
 import cofre.*;
 import mapa.Mapa;
 import org.junit.Before;
 import org.junit.Test;
 import robopuerto.Robopuerto;
 import robot.Robot;
-import utils.DatosJson;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -45,16 +43,9 @@ public class EstacionRobotTest {
         estacion.setup();
 
         ByteArrayOutputStream salidaCapturada = new ByteArrayOutputStream();
-        PrintStream salidaOriginal = System.out;
         System.setOut(new PrintStream(salidaCapturada));
 
         estacion.atenderPedidos();
-
-        System.setOut(salidaOriginal);
-
-        String salida = salidaCapturada.toString();
-        assertTrue("Debe imprimirse que el cofre está fuera de cobertura",
-                salida.contains("El cofre no tiene a ningun robopuerto en cobertura"));
 
         assertFalse(estacion.pedidosNoCumplidos.isEmpty());
     }
@@ -65,6 +56,23 @@ public class EstacionRobotTest {
         Robot robot = new Robot(1, 5, 5, 100, 1.0);
 
         CofreSolicitud cofreS = new CofreSolicitud(6, 6, 1, List.of(item));
+
+        EstacionRobot estacion = new EstacionRobot(mapa, List.of(r), List.of(robot));
+        estacion.addRequestChest(cofreS);
+
+        estacion.setup();
+        estacion.atenderPedidos();
+
+        assertFalse(estacion.pedidosNoCumplidos.isEmpty());
+    }
+
+    @Test
+    public void solicitudAccesibleProveedorNo() {
+        Robopuerto r = new Robopuerto(5, 5, 1);
+        Robot robot = new Robot(1, 5, 5, 100, 1.0);
+
+        CofreSolicitud cofreS = new CofreSolicitud(6, 6, 1, List.of(item));
+        CofreProvisionActiva cofrePA = new CofreProvisionActiva(18, 18, 1,Map.of(item.getNombre(),item.getCantidad()));
 
         EstacionRobot estacion = new EstacionRobot(mapa, List.of(r), List.of(robot));
         estacion.addRequestChest(cofreS);
